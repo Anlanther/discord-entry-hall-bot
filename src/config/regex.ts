0bccb126a9ce8f354efa09f295c0447e72722fb4
anlanther <anlanther@gmail.com>
2026-07-01 21:43:01 +0800

    feat: added any and email to regex

--- src/config/regex.ts
+++ src/config/regex.ts
@@ -1,6 +1,6 @@
 // Terms: woman, she, her, they, them
-export const TERMS_REGEX: RegExp = /\b(woman|she|her|they|them)\b/i;
+export const TERMS_REGEX: RegExp = /\b(woman|she|her|they|them|any)\b/i;
 
 // Source detection
 export const SOURCE_REGEX: RegExp =
-  /\b(?:twitter|fair|ig|friend|browser|online|x|insta|reel(?:s)?|instagram|google|facebook|tiktok|youtube|reddit|snapchat|linkedin|pinterest|threads|discord)\b[\w-]*(?::\/\/)?[\w.-]*|@[\w.-]{2,}|social\s+media|chatgpt|googling|searching/i;
+  /\b(?:twitter|email|fair|ig|friend|browser|online|x|insta|reel(?:s)?|instagram|google|facebook|tiktok|youtube|reddit|snapchat|linkedin|pinterest|threads|discord)\b[\w-]*(?::\/\/)?[\w.-]*|@[\w.-]{2,}|social\s+media|chatgpt|googling|searching/i;
